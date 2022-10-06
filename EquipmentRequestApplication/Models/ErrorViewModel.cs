7c408a3cbfd52ae1883a6948dc39b7ba09644028
Nicholas Cammisuli <cammisun@sheridancollege.ca>
2022-10-05 22:55:40 -0400

    project initialized

---
+++ EquipmentRequestApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EquipmentRequestApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
